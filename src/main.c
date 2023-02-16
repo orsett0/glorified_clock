3778bcb4599fdc0e37513d18a88cbd98f1d9e4da
Alessio Orsini <alessiorsini.ao@gmail.com>
2023-02-16 17:41:48 +0100

    IT BLINKS THE LED BLINKS
    Well the problem was that i was using the wrong binary & command to
    upload the hex.
    Use the binary specific for the raspberry and the command has to be "p14
    program file.hex" without the LVP keyword. That's counter intuitive.
    (TBH the guide was clear about that)

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
-void myfunc() {
+#include <xc.h>
+#include <proc/pic16f887.h>
 
-}
+#pragma config CONFIG1 = 0x3CF4
+#pragma config CONFIG2 = 0x0600
 
-int main() {
-    return 0;
+#define _XTAL_FREQ 8000000
+
+int main()
+{
+  TRISB0 = 0; //RB0 as Output PIN
+  while(1)
+  {
+    RB0 = 1; //LED ON
+    __delay_ms(1000); //3 Second Delay
+    RB0 = 0; //LED OFF
+    __delay_ms(1000);
+  }
+  return 0;
 }
