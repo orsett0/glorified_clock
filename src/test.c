bd0e686455d327a849e7374e1b48082b094d030f
Alessio Orsini <35498861+orsett0@users.noreply.github.com>
2023-03-01 14:31:46 +0100

    interrupt test working.
    Turns out the problem was the LED that I put on the interrupt, taking it
    at ~1.7 V, that should be an undefined state (anyway it wan't high, and
    i needed it high.)

--- src/test.c
+++ src/test.c
@@ -1,25 +1,57 @@
+#pragma config FOSC=INTRC_NOCLKOUT
+#pragma config WDTE=OFF
+#pragma config PWRTE=ON
+#pragma config MCLRE=OFF
+#pragma config CP=OFF
+#pragma config CPD=OFF
+#pragma config BOREN=OFF
+#pragma config IESO=OFF
+#pragma config FCMEN=OFF
+#pragma config LVP=ON
+#pragma config DEBUG=OFF
+
+#pragma config BOR4V=BOR21V
+#pragma config WRT=OFF
+
+
 #include <xc.h>
 #include <proc/pic16f887.h>
 
-#include <stdint.h>
 
-#pragma config CONFIG1 = 0x3CF4
-#pragma config CONFIG2 = 0x0600
+/*
+ * The interrupts are described at page 216, section 14.6 of the PIC16F887 datasheets
+ * And at page 131, section 4.9 of the XC8 guide.
+*/
+#ifdef __XC8 // vscode does not support xc8
+void __interrupt() handler(void) {
+#else
+void handler() {
+#endif
+    if (!INTF) return;
 
-#define _XTAL_FREQ 8000000
-
-int main()
-{
-    uint8_t state = 0;
+    RD4 = 1;
+    __delay_ms(100);
+    RD4 = 0;
     
+    INTF = 0;
+    RBIF = 0;
+}
+
+int main() {
+    //__delay_us(100);
+
+    ANSELH = 0;
+    INTEDG = 0;
+    INTE = 1;
+    ei();
+
     TRISD4 = 0;
+    RD4 = 0;
+    RD4 = 1;
+    __delay_ms(200);
+    RD4 = 0;
+
+    while (1) {}
     
-    while (1) {
-        RD4 = state;
-        state = !state;
-        __delay_ms(100);
-    }
-
-
     return 0;
 }
--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 dd if=$1 bs=1024 | ssh -p42000 -l pi "$2" '
 dd of=/tmp/program.hex
+
 /opt/pickle/p14 program /tmp/program.hex
 /opt/pickle/p14 verify /tmp/program.hex
 '
-    
