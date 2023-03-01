cc55141f8a4cc5ca299afa1064cfa32d154b1168
Alessio Orsini <35498861+orsett0@users.noreply.github.com>
2023-03-02 00:50:14 +0100

    Unique buffer for lcd & removed atomic access to variable.
    I'm using an unique buffer to print on the LCD. It wasn't strictly
    necessary but it's useful to not use too much memory.
    
    Also, I removed the functions to access the variables after disabling
    the interrupts, because this would only be useful if the entire
    comparisions were made in an atomic way.

--- src/test.c
+++ src/test.c
@@ -1,10 +1,3 @@
-// these are redefined at compile time,
-// but if i don't do this vscode screams.
-#ifndef SEC_SINCE_EPOCH
-    #define SEC_SINCE_EPOCH 0
-#endif
-
-
 #pragma config FOSC=INTRC_NOCLKOUT
 #pragma config WDTE=OFF
 #pragma config PWRTE=ON
@@ -33,7 +26,11 @@
 #include "ds1302/ds1302.h"
 #include "defines.h"
 
-
+// these are redefined at compile time,
+// but if i don't do this vscode screams.
+#ifndef SEC_SINCE_EPOCH
+    #define SEC_SINCE_EPOCH 0
+#endif
 #define N_DATES 3
 #define SPACE_PADDING 3
 
@@ -41,11 +38,15 @@
 void printDiff(time_t);
 void printCurtime(time_t curtime);
 inline void flash();
-void createCircularString(char *, char *, uint8_t, short);
-inline uint8_t get(void *p);
-inline uint8_t set(void *p, uint8_t);
-inline uint8_t inc(void *p);
-inline uint8_t dec(void *p);
+void createCircularString(char *, uint8_t, short);
+/*
+ * These aren't useful. They would be only 
+ * if they'd permit atomic comparisons between variables
+*/
+// inline uint8_t get(void *p);
+// inline uint8_t set(void *p, uint8_t);
+// inline uint8_t inc(void *p);
+// inline uint8_t dec(void *p);
 
 
 // Use 'date -d"YYYY-MM-DD hh:mm:ss UTC+1" "+%s"' to populate the dates array.
@@ -61,6 +62,7 @@ char *descs[N_DATES] = {
 };
 // Using signed char so that getter and setter works with the conversion to (uint8_t *)
 int8_t index = -1;
+char buff[LCD_WIDTH + 1];
 
 int8_t overflow;
 int8_t iter;
@@ -95,7 +97,6 @@ void handler() {
 
 
 int main() {
-    char buff[LCD_WIDTH + 1];
     time_t curtime;
     uint8_t len;
 
@@ -132,38 +133,38 @@ int main() {
         curtime = SEC_SINCE_EPOCH; //recvDateTime();
 
         // The interrupt fired. Adjust to the new state and go on.
-        if (get(&int_exec_flag)) {
-            set(&int_exec_flag, 0);
+        if (int_exec_flag) {
+            int_exec_flag = 0;
 
-            if ((int8_t) get(&index) < 0) {
+            if (index < 0) {
                 printCurtime(curtime);
                 continue;
             }
 
             // No need to check if overflow this time
             clear();
-            print(descs[(int8_t) get(&index)]);
-            printDiff(difftime(curtime, dates[(int8_t) get(&index)]));
+            print(descs[index]);
+            printDiff(difftime(curtime, dates[index]));
 
             continue;
         }
 
         __delay_ms(500); // Find the right value.
 
-        if ((int8_t) get(&index) < 0)
+        if (index < 0)
             printCurtime(curtime);
-        else if ((int8_t) get(&overflow) <= 0)
-            printDiff(difftime(curtime, dates[(int8_t) get(&index)]));
+        else if (overflow <= 0)
+            printDiff(difftime(curtime, dates[index]));
         else {
-            if ((int8_t) get(&iter) == (int8_t) get(&overflow) + LCD_WIDTH) set(&iter, -SPACE_PADDING);
+            if (iter == overflow + LCD_WIDTH) iter = -SPACE_PADDING;
 
-            createCircularString(buff, descs[(int8_t) get(&index)], strlen(descs[(int8_t) get(&index)]), (int8_t) inc(&iter));
+            createCircularString(descs[index], strlen(descs[index]), ++iter);
             buff[LCD_WIDTH] = '\0';
 
             moveCursor(0, 0);
             print(buff);
             
-            printDiff(difftime(curtime, dates[(int8_t) get(&index)]));
+            printDiff(difftime(curtime, dates[index]));
         }
     }
     
@@ -171,11 +172,37 @@ int main() {
 }
 
 
-// There's probably a better way to do all of this
-void createCircularString(char *buff, char *src, uint8_t src_len, short src_offset) {
+/*
+ * It is buggy. like a lot. don't ever try to change the number of spaces. (or anything else)
+ * And don't ever even think to have a problem with this function. You'll regret all of you life choices.
+ * If you need to change something, rewrite it from scratch. You'll figure something out.
+*/
+void createCircularString(char *src, uint8_t src_len, short src_offset) {
     uint8_t to_copy;
 
-    if (buff == NULL || src == NULL || src_len == 0) return;
+    if (src == NULL || src_len == 0) return;
+
+    /*
+     * Need to do this cause the last padding space doesn't get overwritten,
+     * and i don't know what to change to make it work. Forgive me I'm sorry.
+    */
+    buff[0] = ' ';
+    buff[1] = ' ';
+    buff[2] = ' ';
+    buff[3] = ' ';
+    buff[4] = ' ';
+    buff[5] = ' ';
+    buff[6] = ' ';
+    buff[7] = ' ';
+    buff[8] = ' ';
+    buff[9] = ' ';
+    buff[10] = ' ';
+    buff[11] = ' ';
+    buff[12] = ' ';
+    buff[13] = ' ';
+    buff[14] = ' ';
+    buff[15] = ' ';
+    buff[16] = ' ';
 
     if (src_offset < 0) {
         strncpy(buff - src_offset, src, LCD_WIDTH + src_offset);
@@ -203,9 +230,7 @@ inline void flash() {
 
 void printDiff(time_t diff) {
     struct tm *datetime = localtime(&diff);
-    char buff[41];
 
-    // no need to clear if printing the whole line
     moveCursor(1, 0);
 
     // Sub 70 from the year because epoch is 1970 but time.h gives the years passed since 1900
@@ -215,51 +240,47 @@ void printDiff(time_t diff) {
 
 void printCurtime(time_t curtime) {
     struct tm *datetime = localtime(&curtime);
-    char buff[17];
 
-    // no need to clear if printing the whole lines
-    clear();
-
+    moveCursor(0, 0);
     sprintf(buff, "   %02d/%02d/20%02d   \0", datetime->tm_mday, datetime->tm_mon, datetime->tm_year - 100);
     print(buff);
 
     moveCursor(1, 0);
-
     sprintf(buff, "    %02d:%02d:%02d    \0", datetime->tm_hour, datetime->tm_min, datetime->tm_sec);
     print(buff);
 }
 
-inline uint8_t get(void *p) {
-    uint8_t value;
+// inline uint8_t get(void *p) {
+//     uint8_t value;
 
-    di();
-    value = *((uint8_t *) p);
-    ei();
+//     di();
+//     value = *((uint8_t *) p);
+//     ei();
 
-    return value;
-}
-inline uint8_t set(void *p, uint8_t value) {
-    di();
-    *((uint8_t *) p) = value;
-    ei();
+//     return value;
+// }
+// inline uint8_t set(void *p, uint8_t value) {
+//     di();
+//     *((uint8_t *) p) = value;
+//     ei();
 
-    return value;
-}
-inline uint8_t inc(void *p) {
-    uint8_t value;
-    di();
-    *((uint8_t *) p) = *((uint8_t *) p) + 1;
-    value = *((uint8_t *) p);
-    ei();
+//     return value;
+// }
+// inline uint8_t inc(void *p) {
+//     uint8_t value;
+//     di();
+//     *((uint8_t *) p) = *((uint8_t *) p) + 1;
+//     value = *((uint8_t *) p);
+//     ei();
 
-    return value;
-}
-inline uint8_t dec(void *p) {
-    uint8_t value;
-    di();
-    *((uint8_t *) p) = *((uint8_t *) p) - 1;
-    value = *((uint8_t *) p);
-    ei();
+//     return value;
+// }
+// inline uint8_t dec(void *p) {
+//     uint8_t value;
+//     di();
+//     *((uint8_t *) p) = *((uint8_t *) p) - 1;
+//     value = *((uint8_t *) p);
+//     ei();
 
-    return value;
-}
+//     return value;
+// }
