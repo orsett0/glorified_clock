17b8397582e6d06f25a0c6be0fc550065c9fe3af
Alessio Orsini <35498861+orsett0@users.noreply.github.com>
2023-04-12 22:07:11 +0200

    Add src/includes/dates.h
    I made a series of oopsies with this file lol

---
+++ src/include/dates.h
@@ -0,0 +1,29 @@
+#ifndef DATES_H
+#define DATES_H
+
+#include <time.h>
+
+#define N_DATES         22
+
+// Use 'date -d"YYYY-MM-DD hh:mm:ss" "+%s"' to populate the dates array.
+// add 3600 for CET and 7200 for CEST
+#ifdef __XC8
+__eeprom time_t dates[N_DATES] = {
+#else
+time_t dates[N_DATES] = {
+#endif
+    1577664000,
+    1580515200,
+    1580688000,
+};
+#ifdef __XC8
+__eeprom char *descs[N_DATES] = {
+#else
+char *descs[N_DATES] = {
+#endif
+    "Data 1",
+    "Data 2",
+    "Altra data con descrizione lunga",
+};
+
+#endif
